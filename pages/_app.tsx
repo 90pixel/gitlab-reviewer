f4fcbd440026b1206fef457f4ddaadd90b63c920
Burak <burak@90pixel.com>
2021-11-25 23:17:06 +0300

    Add global style to app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app';
 import 'styles/libraryStyles.css';
+import { globalStyles } from 'styles/globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles();
   return <Component {...pageProps} />;
 }
 
