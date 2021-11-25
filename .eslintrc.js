2860b7b88fe1f3e3668eb78c993f896d37198993
Burak <burak@90pixel.com>
2021-11-25 20:18:31 +0300

    Configure app.

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'jsx-a11y/alt-text': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-expressions': 'warn',
+    'no-unused-labels': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/self-closing-comp': 'error',
+    'no-undef': 'error',
+    'prettier/prettier': 'error',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+        '@typescript-eslint/restrict-template-expressions': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@next/next/no-img-element': 'off',
+        '@typescript-eslint/require-await': 'off',
+      },
+    },
+  ],
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Gitlab reviewer</div>;
+  return (
+    <div>
+      <span
+        className="112"
+        style={{
+          textAlign: 'center',
+          fontSize: 12,
+          borderColor: 'red',
+          borderWidth: 2,
+        }}
+      >
+        XXxx
+      </span>
+    </div>
+  );
 };
 
 export default Home;
