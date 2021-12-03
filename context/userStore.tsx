7150bf26274e958e58838edd43b885a5513ced8e
Burak <burak@90pixel.com>
2021-12-04 01:44:33 +0300

    Add userStore.

---
+++ context/userStore.tsx
@@ -0,0 +1,36 @@
+import { createContext, useState, useEffect, FC } from 'react';
+import { parseCookies } from 'nookies';
+import { USER } from 'types/USER';
+import { endpoints, fetcher } from 'utils';
+
+interface IUserContext {
+  user: USER | null;
+}
+const UserContext = createContext<IUserContext>({ user: null });
+
+interface IUserProvider {
+  children?: React.ReactNode;
+}
+
+const UserProvider: FC<IUserProvider> = ({ children }) => {
+  const [user, setUser] = useState<USER | null>(null);
+  const { gitlabUrl, privateToken } = parseCookies();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await fetcher({ resource: endpoints.user });
+      console.log(response);
+    };
+    getUser();
+  }, [gitlabUrl, privateToken]);
+
+  const values = { user, setUser };
+
+  return (
+    <UserContext.Provider value={{ ...values }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContext, UserProvider };
