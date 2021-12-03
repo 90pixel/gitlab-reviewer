48c7b6e232fda2b4fa3dbf871cdfc53716d1c920
Burak <burak@90pixel.com>
2021-12-04 01:44:56 +0300

    Connect context to app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,15 @@ import type { AppProps } from 'next/app';
 import 'styles/libraryStyles.css';
 import { globalStyles } from 'styles/globals';
 
+import { Providers } from 'context';
+
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
-  return <Component {...pageProps} />;
+  return (
+    <Providers>
+      <Component {...pageProps} />;
+    </Providers>
+  );
 }
 
 export default MyApp;
