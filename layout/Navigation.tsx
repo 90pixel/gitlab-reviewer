c6419218740b0091c95e94e767a4798b1ae7d32e
Burak <burak@90pixel.com>
2021-12-04 22:48:38 +0300

    Add user avatar to navigation.

--- layout/Navigation.tsx
+++ layout/Navigation.tsx
@@ -1,12 +1,26 @@
 import Link from 'next/link';
+import { Avatar, Badge, Tooltip } from 'antd';
 import { styled } from 'stitches.config';
+import { useUser } from 'hooks';
 
 const Navigation = () => {
+  const { user } = useUser();
+  const firstLatters = user?.name
+    .split(' ')
+    .map((name) => name[0])
+    .join('');
+
   return (
     <StyledNavigation>
       <Link href="/" passHref>
         <a>Home</a>
       </Link>
+
+      <Tooltip title={user?.name}>
+        <Badge count={2}>
+          <Avatar src={user?.avatar_url}>{firstLatters}</Avatar>
+        </Badge>
+      </Tooltip>
     </StyledNavigation>
   );
 };
