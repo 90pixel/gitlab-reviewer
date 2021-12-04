96db8018c35174c98a81417dee8e7e2fb42dbce4
Burak <burak@90pixel.com>
2021-12-04 19:22:06 +0300

    Refactor fetcher method.

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,98 +1,6 @@
 // /* eslint-disable max-len */
 import { parseCookies } from 'nookies';
 
-// import Router from 'next/router';
-// /**
-//  * A fetcher function for all of your api interactions. You can also use this with `useSWR` hook.
-//  * @typedef Props
-//  * @property {boolean} [json] Sets the `Content-Type` header to `application/json`. Default: true
-//  * @property {boolean} [retryUnauthenticated] Re-try unauthenticated requests. Default: true
-//  *
-//  * @param {string} resource Must start with slash like "/auth/login"
-//  * @param {RequestInit & Props} init
-//  *
-//  * @example
-//     try {
-//       const response = await fetcher("/auth/login", {
-//         method: "POST",
-//         body: {
-//           email,
-//           password
-//         },
-//       });
-//       console.log(response)
-//     } catch (err) {
-//       console.log(err.info)
-//     }
-//   };
-//  */
-// const fetcher = async (resource, init = {}) => {
-//   const {
-//     accessToken,
-//     refreshToken,
-//     'x-tindex-token': token,
-//   } = parseCookies(null);
-
-//   // Eğer fetcher admin sayfalarında kullanılacaksa tokenlar ona göre ayarlanıyor.
-//   const { asPath } = Router;
-//   const isAdminRoutes = asPath.includes('/admin');
-//   const computedToken = isAdminRoutes ? accessToken : token;
-
-//   const defaults = {
-//     json: true,
-//     auth: true,
-//     retryUnauthenticated: true,
-//     ...init,
-//   };
-
-//   const requestInit = {
-//     ...defaults,
-//     headers: {
-//       ...(defaults.json && { Accept: 'application/json' }),
-//       ...(defaults.json && { 'Content-Type': 'application/json' }),
-//       ...(defaults.auth &&
-//         computedToken && { Authorization: `Bearer ${computedToken}` }),
-//     },
-//     body: defaults.json ? JSON.stringify(defaults.body) : defaults.body,
-//   };
-
-//   const response = await fetch(
-//     `${process.env.NEXT_PUBLIC_BASE_URL}${resource}`,
-//     requestInit
-//   );
-//   // Admin sayfasında istek atıyorsak ve tokenlar yoksa girişe zorla
-//   if (isAdminRoutes && !accessToken && !refreshToken) {
-//     Router.push('/admin');
-//   }
-
-//   if (!response.ok) {
-//     if (response.status === 401 && refreshToken) {
-//       const { token } = await fetcher('/auth/refresh', {
-//         method: 'POST',
-//         retryUnauthenticated: false,
-//         body: { refreshToken },
-//       });
-//       setCookie(null, 'accessToken', token);
-
-//       if (defaults.retryUnauthenticated) {
-//         return fetcher(`${resource}`, {
-//           ...requestInit,
-//           retryUnauthenticated: false,
-//         });
-//       }
-//     }
-//   }
-
-//   try {
-//     const result = await response.json();
-//     return result;
-//   } catch (err) {
-//     return err;
-//   }
-// };
-
-// export default fetcher;
-
 interface IFetcher {
   resource: string;
   customUrl?: string;
@@ -104,14 +12,24 @@ const fetcher = async ({ resource, customUrl, customToken }: IFetcher) => {
   const computedUrl = customUrl || gitlabUrl;
   const computedToken = customToken || privateToken;
 
-  const request = await fetch(`${computedUrl}/api/v4/${resource}`, {
-    headers: {
-      'private-token': computedToken,
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  });
-  console.log(request);
+  try {
+    const request = await fetch(`${computedUrl}/api/v4/${resource}`, {
+      headers: {
+        'private-token': computedToken,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
+    // istekten gelebilecek datayı bilmediğim için
+    // ve any/unknown için bilinmeyen tiplerin diğer yerlerde
+    // set edilmesine izin vermediği için type null belirledim
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const response: null = await request.json();
+    return response;
+  } catch (error) {
+    return error;
+  }
 };
 
 export default fetcher;
