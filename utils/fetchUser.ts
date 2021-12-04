10fbd8f22b32c2930bfdd96dfde3d1dfddbcd78b
Burak <burak@90pixel.com>
2021-12-04 21:52:08 +0300

    Add fetchUser method to prevent duplication.

---
+++ utils/fetchUser.ts
@@ -0,0 +1,25 @@
+import { endpoints } from 'utils';
+import { USER } from 'types/USER';
+
+const fetchUser = async (gitlabUrl: string, token: string) => {
+  try {
+    const request = await fetch(`${gitlabUrl}/api/v4/${endpoints.user}`, {
+      headers: {
+        'private-token': token,
+      },
+    });
+    // JSON için bu kuralı kapatıyorum
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const response: USER = await request.json();
+
+    if (response.name) {
+      return response;
+    } else {
+      return { error: 'Token hatalı' };
+    }
+  } catch {
+    // https://22 gibi veya https://gitlab.123.net gibi şeylerde de bu hatayı alıyoruz.
+    return { error: 'Gitlab Urli hatalı' };
+  }
+};
+export default fetchUser;
--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,3 @@
 export { default as endpoints } from './endpoints';
 export { default as fetcher } from './fetcher';
+export { default as fetchUser } from './fetchUser';
