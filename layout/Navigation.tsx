f65e44e72d7c9a6979486182f24e9a43e1068af0
Burak <burak@90pixel.com>
2021-12-05 00:42:31 +0300

    Cleear cookies when logged out

--- layout/Navigation.tsx
+++ layout/Navigation.tsx
@@ -1,21 +1,32 @@
+import { useRouter } from 'next/router';
+import { destroyCookie } from 'nookies';
 import Link from 'next/link';
 import { Avatar, Badge, Tooltip } from 'antd';
 import { styled } from 'stitches.config';
 import { useUser } from 'hooks';
 
 const Navigation = () => {
+  const { push } = useRouter();
   const { user } = useUser();
+
   const firstLatters = user?.name
     .split(' ')
     .map((name) => name[0])
     .join('');
+
+  const handleLogout = async () => {
+    // TODO: Çıkış yapılınca user contexti de temizlenecek.
+    destroyCookie(null, 'gitlabUrl');
+    destroyCookie(null, 'privateToken');
+    push('/login');
+  };
 
   return (
     <StyledNavigation>
       <Link href="/" passHref>
         <a>Home</a>
       </Link>
-      <button>Çıkış</button>
+      <button onClick={handleLogout}>Çıkış</button>
 
       <Tooltip title={user?.name}>
         <Badge count={2}>
