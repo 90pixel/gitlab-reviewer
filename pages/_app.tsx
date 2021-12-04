c8f9a0a49fa2fa443c05284e421008fa4b0e563d
Burak <burak@90pixel.com>
2021-12-04 22:02:44 +0300

    Add route protectin for page.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import type { AppProps } from 'next/app';
+import App from 'next/app';
+import type { AppProps, AppContext } from 'next/app';
+import nookies from 'nookies';
 import 'styles/libraryStyles.css';
 import { globalStyles } from 'styles/globals';
 
 import { Providers } from 'context';
-
+import { fetchUser } from 'utils';
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
   return (
@@ -14,3 +16,29 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  const { ctx } = appContext;
+  const { gitlabUrl, privateToken } = nookies.get(ctx);
+
+  if (ctx.pathname !== '/login') {
+    if (!gitlabUrl && !privateToken) {
+      ctx?.res?.writeHead(307, {
+        location: '/login',
+      });
+      return ctx?.res?.end();
+    } else {
+      const response = await fetchUser(gitlabUrl, privateToken);
+      if ('error' in response) {
+        ctx?.res?.writeHead(307, {
+          location: '/login',
+        });
+        return ctx?.res?.end();
+      }
+    }
+  }
+
+  return { ...appProps };
+};
