a2d9546cefe21fe22978a156a79bdc2d175f602c
Burak <burak@90pixel.com>
2021-12-04 22:48:28 +0300

    Add useUser hook.

---
+++ hooks/useUser.ts
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import { UserContext } from 'context';
+
+const useUser = () => {
+  const { user } = useContext(UserContext);
+  return { user };
+};
+
+export default useUser;
