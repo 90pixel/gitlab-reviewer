dedfbccc5d7acb4dd0988ca8bf525ec3a94a277b
Burak <burak@90pixel.com>
2021-12-07 23:45:38 +0300

    Fix redirects.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,9 @@ import type { AppProps, AppContext } from 'next/app';
 import nookies from 'nookies';
 import 'styles/libraryStyles.css';
 import { globalStyles } from 'styles/globals';
-
 import { Providers } from 'context';
 import { fetchUser } from 'utils';
+
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
   return (
@@ -19,21 +19,37 @@ export default MyApp;
 
 MyApp.getInitialProps = async (appContext: AppContext) => {
   const appProps = await App.getInitialProps(appContext);
-
   const { ctx } = appContext;
   const { gitlabUrl, privateToken } = nookies.get(ctx);
 
-  if (ctx.pathname !== '/login') {
-    if (!gitlabUrl && !privateToken) {
+  // Eğer private token ve gitlab url yoksa
+  if (!gitlabUrl && !privateToken) {
+    // ve mevcut url login değilse (Çok fazla redirect yapmamak için)
+    // Login sayfasına yönlendir
+    if (ctx.pathname !== '/login') {
       ctx?.res?.writeHead(307, {
         location: '/login',
       });
       return ctx?.res?.end();
-    } else {
-      const response = await fetchUser(gitlabUrl, privateToken);
-      if ('error' in response) {
+    }
+  } else {
+    // Eğer token ve private token varsa user bilgisini çek
+    const response = await fetchUser(gitlabUrl, privateToken);
+    // eğer response içinde error varsa ve mevcut sayfa login değlse logine yönlendir
+    // eğer mevcut sayfa loginse loginde kal
+    if ('error' in response) {
+      if (ctx.pathname !== '/login') {
         ctx?.res?.writeHead(307, {
           location: '/login',
+        });
+        return ctx?.res?.end();
+      }
+    } else {
+      // eğer private token ve gitlab url varolmasına rağmen login sayfasına gitmek istersem
+      // anasayfaya yönlendir
+      if (ctx.pathname === '/login') {
+        ctx?.res?.writeHead(307, {
+          location: '/',
         });
         return ctx?.res?.end();
       }
