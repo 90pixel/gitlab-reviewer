ae52affd1c561042b136f520c13cf9b8cc2622a5
Burak <burak@90pixel.com>
2021-12-07 23:54:35 +0300

    Refactor routeProtection logic.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
-import App from 'next/app';
 import type { AppProps, AppContext } from 'next/app';
-import nookies from 'nookies';
 import 'styles/libraryStyles.css';
 import { globalStyles } from 'styles/globals';
 import { Providers } from 'context';
-import { fetchUser } from 'utils';
+import { routeProtection } from 'utils';
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
@@ -18,43 +16,5 @@ function MyApp({ Component, pageProps }: AppProps) {
 export default MyApp;
 
 MyApp.getInitialProps = async (appContext: AppContext) => {
-  const appProps = await App.getInitialProps(appContext);
-  const { ctx } = appContext;
-  const { gitlabUrl, privateToken } = nookies.get(ctx);
-
-  // Eğer private token ve gitlab url yoksa
-  if (!gitlabUrl && !privateToken) {
-    // ve mevcut url login değilse (Çok fazla redirect yapmamak için)
-    // Login sayfasına yönlendir
-    if (ctx.pathname !== '/login') {
-      ctx?.res?.writeHead(307, {
-        location: '/login',
-      });
-      return ctx?.res?.end();
-    }
-  } else {
-    // Eğer token ve private token varsa user bilgisini çek
-    const response = await fetchUser(gitlabUrl, privateToken);
-    // eğer response içinde error varsa ve mevcut sayfa login değlse logine yönlendir
-    // eğer mevcut sayfa loginse loginde kal
-    if ('error' in response) {
-      if (ctx.pathname !== '/login') {
-        ctx?.res?.writeHead(307, {
-          location: '/login',
-        });
-        return ctx?.res?.end();
-      }
-    } else {
-      // eğer private token ve gitlab url varolmasına rağmen login sayfasına gitmek istersem
-      // anasayfaya yönlendir
-      if (ctx.pathname === '/login') {
-        ctx?.res?.writeHead(307, {
-          location: '/',
-        });
-        return ctx?.res?.end();
-      }
-    }
-  }
-
-  return { ...appProps };
+  return routeProtection(appContext);
 };
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
 export { default as endpoints } from './endpoints';
 export { default as fetcher } from './fetcher';
 export { default as fetchUser } from './fetchUser';
+export { default as routeProtection } from './routeProtection';
