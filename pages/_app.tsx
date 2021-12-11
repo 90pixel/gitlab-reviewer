13ea3798e864fe58836c9ff4b45f87a8489d6b6b
Burak <burak@90pixel.com>
2021-12-12 00:51:24 +0300

    Add config provider to make antd lang locale tr.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps, AppContext } from 'next/app';
 import { SWRConfig } from 'swr';
 import 'styles/libraryStyles.css';
 import { globalStyles } from 'styles/globals';
+import { ConfigProvider } from 'antd';
+import trTR from 'antd/lib/locale/tr_TR';
 import { Providers } from 'context';
 import { routeProtection, fetcher } from 'utils';
 
@@ -9,11 +11,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
 
   return (
-    <SWRConfig value={{ fetcher }}>
-      <Providers>
-        <Component {...pageProps} />;
-      </Providers>
-    </SWRConfig>
+    <ConfigProvider locale={trTR}>
+      <SWRConfig value={{ fetcher }}>
+        <Providers>
+          <Component {...pageProps} />;
+        </Providers>
+      </SWRConfig>
+    </ConfigProvider>
   );
 }
 
