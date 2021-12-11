320d1dccc41771ed38d5c41c87e67097069668af
Burak <burak@90pixel.com>
2021-12-11 23:13:34 +0300

    Add my request container at main page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,11 @@
 import type { NextPage } from 'next';
-import swr from 'swr';
 import Layout from 'layout';
-import { Table } from 'components';
-import { USER } from 'types/USER';
+import { MyRequests } from 'container';
 
-interface IData {
-  data?: USER[];
-}
 const Dashboard: NextPage = () => {
-  const dataSource = [
-    {
-      key: '1',
-      name: 'Mike',
-      age: 32,
-      address: '10 Downing Street',
-    },
-    {
-      key: '2',
-      name: 'John',
-      age: 42,
-      address: '10 Downing Street',
-    },
-  ];
-
-  const columns = [
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-  ];
-
-  const { data }: IData = swr('/users');
-
   return (
     <Layout>
-      <Table dataSource={dataSource} columns={columns} />
+      <MyRequests />
     </Layout>
   );
 };
