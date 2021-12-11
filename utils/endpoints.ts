dd83090479fa059b91d1320b04d9ba53c634ed51
Burak <burak@90pixel.com>
2021-12-11 23:14:30 +0300

    Add request endpoint.

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -1,6 +1,16 @@
+/* eslint-disable max-len */
+interface IRequest {
+  reviewer_id: number;
+  sort?: 'asc' | 'desc';
+  page: number;
+  perPage: number;
+  updatedAt: string;
+  updatedBefore: string;
+}
 const endpoints = {
-  mainUrl: `../api/v4`,
   user: `/user`,
+  request: (options?: IRequest) =>
+    '/merge_requests?scope=all&order_by=updated_at&sort=desc&view=simple&page=1&per_page=100&reviewer_id=80&updated_after=2021-11-29T21:00:00.000Z&updated_before=2021-12-31T20:59:59.999Z',
 };
 
 export default endpoints;
