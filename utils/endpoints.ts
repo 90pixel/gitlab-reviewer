bc3a91fe1e22db4e8bc4990226d7e3af249c5fd6
Burak <burak@90pixel.com>
2021-12-12 00:51:03 +0300

    Add request detail endpoint.

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -7,10 +7,17 @@ interface IRequest {
   updatedAt: string;
   updatedBefore: string;
 }
+
+interface IRequestDetail {
+  project_id: number;
+  iid: number;
+}
 const endpoints = {
   user: `/user`,
   request: (options?: IRequest) =>
     '/merge_requests?scope=all&order_by=updated_at&sort=desc&view=simple&page=1&per_page=100&reviewer_id=80&updated_after=2021-11-29T21:00:00.000Z&updated_before=2021-12-31T20:59:59.999Z',
+  requestDetail: (options?: IRequestDetail) =>
+    `/projects/${options?.project_id}/merge_requests/${options?.iid}`,
 };
 
 export default endpoints;
