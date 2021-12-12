3855da2161d1848517ee5790dc6b9ce67d2e989b
Burak <burak@90pixel.com>
2021-12-13 00:32:29 +0300

    Add new table props.

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,16 +1,34 @@
 import { FC } from 'react';
-import { Table as AntTable } from 'antd';
+import { Table as AntTable, TablePaginationConfig } from 'antd';
 import { ColumnsType } from 'antd/lib/table';
 
 interface ITable {
   dataSource: any[];
   columns: ColumnsType<any>;
   loading?: boolean;
+  totalPages?: number;
+  pageSize?: number;
+  currentPage?: number;
+  onChange?: (e: TablePaginationConfig) => void;
 }
 
-const Table: FC<ITable> = ({ dataSource, columns, loading }) => {
+const Table: FC<ITable> = ({
+  dataSource,
+  columns,
+  loading,
+  totalPages,
+  pageSize,
+  currentPage,
+  onChange,
+}) => {
   return (
-    <AntTable dataSource={dataSource} columns={columns} loading={loading} />
+    <AntTable
+      dataSource={dataSource}
+      columns={columns}
+      loading={loading}
+      pagination={{ total: totalPages, pageSize, current: currentPage }}
+      onChange={onChange}
+    />
   );
 };
 
