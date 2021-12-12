0b0751e938544b64ee41ea3a7894cd140e19265d
Burak <burak@90pixel.com>
2021-12-13 00:45:56 +0300

    Get my open merge request count.

--- layout/Navigation.tsx
+++ layout/Navigation.tsx
@@ -1,13 +1,20 @@
+import swr, { SWRResponse } from 'swr';
 import { useRouter } from 'next/router';
 import { destroyCookie } from 'nookies';
 import Link from 'next/link';
 import { Avatar, Badge, Tooltip } from 'antd';
 import { styled } from 'stitches.config';
 import { useUser } from 'hooks';
+import { endpoints } from 'utils';
+import { RequestType } from 'types/REQUESTS';
+interface IResponse {
+  response: RequestType[];
+}
 
 const Navigation = () => {
   const { push } = useRouter();
   const { user, userDispatch } = useUser();
+  const { data }: SWRResponse<IResponse> = swr(endpoints.myOpenRequests);
 
   const firstLatters = user?.name
     .split(' ')
@@ -31,7 +38,7 @@ const Navigation = () => {
       <button onClick={handleLogout}>Çıkış</button>
 
       <Tooltip title={user?.name}>
-        <Badge count={2}>
+        <Badge count={data?.response?.length}>
           <Avatar src={user?.avatar_url}>{firstLatters}</Avatar>
         </Badge>
       </Tooltip>
