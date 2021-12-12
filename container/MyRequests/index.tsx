c14bf79cd4f2e78946d824338080f26b70d46d32
Burak <burak@90pixel.com>
2021-12-13 00:33:33 +0300

    Improve requests table.

--- container/MyRequests/index.tsx
+++ container/MyRequests/index.tsx
@@ -1,65 +1,73 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import swr, { SWRResponse } from 'swr';
 import { Table } from 'components';
-import { endpoints, fetcher } from 'utils';
+import { PersonCell } from 'components/TableCells';
+import { endpoints } from 'utils';
+import { useUser } from 'hooks';
 import { RequestType } from 'types/REQUESTS';
 
+const columns = [
+  {
+    title: 'Yazar',
+    dataIndex: 'author',
+    key: 'author',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+
+interface IResponse {
+  response: RequestType[];
+  totalPages: number;
+  total: number;
+}
+
 const Dashboard: FC = () => {
-  const columns = [
-    {
-      title: 'Author',
-      dataIndex: 'author',
-      key: 'author',
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-    },
-    {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    },
-  ];
+  const { user } = useUser();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(10);
 
-  const { data }: SWRResponse<RequestType[]> = swr(endpoints.request());
-  const [test, setTest] = useState<RequestType[] | []>([]);
-  const [loading, setLoading] = useState<boolean>(true);
+  const { data, error }: SWRResponse<IResponse, Error> = swr(
+    endpoints.request({
+      per_page: perPage,
+      page: currentPage,
+      ...(user && { reviewer_id: user.id }),
+    })
+  );
 
-  useEffect(() => {
-    const getDetails = async () => {
-      if (data) {
-        const pool: RequestType[] = [];
-        for (const request of data) {
-          const { project_id, iid } = request;
-          const requestDetail: RequestType = await fetcher(
-            endpoints.requestDetail({
-              project_id,
-              iid,
-            })
-          );
-          pool.push(requestDetail);
+  const renderTable = data && (
+    <Table
+      dataSource={data?.response?.map((item) => ({
+        ...item,
+        author: <PersonCell person={item.author} />,
+      }))}
+      columns={columns}
+      loading={!data && !error}
+      pageSize={perPage}
+      totalPages={data?.total}
+      currentPage={currentPage}
+      onChange={(e) => {
+        if (e.current && e.pageSize) {
+          setCurrentPage(e.current);
+          setPerPage(e.pageSize);
         }
-        setTest(pool);
-        setLoading(false);
-      }
-    };
-
-    getDetails();
-  }, [data]);
-
+      }}
+    />
+  );
   return (
     <div>
-      {test && (
-        <Table
-          dataSource={test?.map((item) => ({
-            ...item,
-            author: item.author.name,
-          }))}
-          columns={columns}
-          loading={loading}
-        />
+      {!data && !error ? (
+        <Table dataSource={[]} columns={columns} loading />
+      ) : (
+        renderTable
       )}
     </div>
   );
