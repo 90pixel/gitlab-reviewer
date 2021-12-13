2cab8b3f222383f678ca92a129bbacd791dc6bad
Burak <burak@90pixel.com>
2021-12-13 23:05:30 +0300

    Add new columns to table.

--- container/MyRequests/index.tsx
+++ container/MyRequests/index.tsx
@@ -2,25 +2,41 @@ import { FC, useState } from 'react';
 import swr, { SWRResponse } from 'swr';
 import { Table } from 'components';
 import { PersonCell } from 'components/TableCells';
-import { endpoints } from 'utils';
+import { endpoints, dateFormat } from 'utils';
 import { useUser } from 'hooks';
 import { RequestType } from 'types/REQUESTS';
 
 const columns = [
+  {
+    title: 'Başlık',
+    dataIndex: 'title',
+    key: 'title',
+    width: 300,
+  },
+  {
+    title: 'Repository',
+    dataIndex: 'repo',
+    key: 'repo',
+  },
   {
     title: 'Yazar',
     dataIndex: 'author',
     key: 'author',
   },
   {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
+    title: 'Reviewer',
+    dataIndex: 'reviewer',
+    key: 'reviewer',
   },
   {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
+    title: 'Oluşturulma Tarihi',
+    dataIndex: 'created_at',
+    key: 'created_at',
+  },
+  {
+    title: 'Mergelenme Tarihi',
+    dataIndex: 'merged_at',
+    key: 'merged_at',
   },
 ];
 
@@ -48,6 +64,13 @@ const Dashboard: FC = () => {
       dataSource={data?.response?.map((item) => ({
         ...item,
         author: <PersonCell person={item.author} />,
+        reviewer: <PersonCell person={item.reviewers[0]} />,
+        created_at: dateFormat(item.created_at),
+        merged_at: item.merged_at ? dateFormat(item.merged_at) : '-',
+        // örnek repo adı: Front-end/xxx!192
+        // Repo adında !192 referans kodunu ifade ediyor.
+        // Bu isimden referans kodunu çıkarıyorum ve splash arasını açıyorum
+        repo: item.references.full.split('!')[0].replace('/', ' / '),
       }))}
       columns={columns}
       loading={!data && !error}
