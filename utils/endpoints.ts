e4b20478559af138d94636462466108705c5948a
Burak <burak@90pixel.com>
2022-01-23 00:37:35 +0300

    Refactor endpoints util.

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -1,35 +1,27 @@
 /* eslint-disable max-len */
 
-const handleParams = (options: object | undefined) => {
-  if (options) {
-    const keys = Object.keys(options);
-    const vals = Object.values(options);
-    const params = keys.map((key, index) => `&${key}=${vals[index]}`);
-    return params.join('');
-  }
-};
-
-interface IRequest {
-  reviewer_id?: number;
+type IRequest = {
+  reviewer_id?: string;
+  page?: string;
+  per_page?: string;
   sort?: 'asc' | 'desc';
-  page?: number;
-  per_page?: number;
   updated_after?: string;
   updated_before?: string;
-}
+};
 
-// interface IRequestDetail {
-//   project_id: number;
-//   iid: number;
-// }
+const handleParams = (options: IRequest | undefined): string => {
+  return new URLSearchParams(options).toString();
+};
+
 const endpoints = {
   user: `/user`,
-
+  users: `/users?active=true&per_page=2000`, // tüm kullanıcıları çekmek için per_page'i kendimiz veriyoruz ve aktif olanları çekmek için de o durumu isteğe ekliyoruz.
   request: (options?: IRequest) =>
-    `/merge_requests?scope=all${handleParams(options)}`,
+    `/merge_requests?scope=all&${handleParams(options)}`,
   myOpenRequests: '/merge_requests?state=opened&scope=assigned_to_me', // Bana açılmış ve açık durumda olan requestler. Headerda kullanmak için var
   // requestDetail: (options?: IRequestDetail) =>
   //   `/projects/${options?.project_id}/merge_requests/${options?.iid}`,
+  // members: 'groups/:id/members',
 };
 
 export default endpoints;
